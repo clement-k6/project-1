9ad6e8f86646d2a5bc6e58418556b23dfcd6f3cb
clement-k6 <kibuchiclement5362680>
2025-03-12 01:03:27 +0300

    My Portfolio

---
+++ script.js
@@ -0,0 +1,25 @@
+// JavaScript for opening messages or content on button click
+
+// Function to display Contacts Page
+document.querySelector('.Contacts-btn').addEventListener('click', function() {
+  window.location.href = 'contacts.html'; // Redirects to the contacts page
+});
+
+// Function to display About Page Page
+document.querySelector('.About-page-btn').addEventListener('click', function() {
+  window.location.href = 'aboutpage.html'; // Redirects to the about page
+  });
+  
+  // Function to display home Page
+  document.querySelector('.Home-btn').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Redirects to the home page
+  });
+  
+
+  // Function to display My Achievements Page
+  document.querySelector('.My-Achievements-btn').addEventListener('click', function() {
+    window.location.href = 'my achievements.html'; // Redirects to the My Achievements page
+  });
+  
+
+  
